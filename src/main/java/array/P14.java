f50bc2c5d6bdf4e1e4d5f8918befd722e1eb7501
harish-sharma <harish.sharma@inmobi.com>
2015-04-07 21:45:31 +0530

    Some

---
+++ src/main/java/array/P14.java
@@ -0,0 +1,50 @@
+package array;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Given an array which is sorted, but after sorting some elements are moved to either of the adjacent positions, i.e.,
+ * arr[i] may be present at arr[i+1] or arr[i-1]. Write an efficient function to search an element in this array.
+ * Basically the element arr[i] can only be swapped with either arr[i+1] or arr[i-1].
+ * 
+ * @author harish.sharma
+ *
+ */
+@Slf4j
+public class P14 {
+
+    public boolean solve(int[] a, int k) {
+        int start = 0;
+        int end = a.length - 1;
+        return search(a, start, end, k);
+    }
+
+    private boolean search(int[] a, int start, int end, int k) {
+        if (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (a[mid] < k) {
+                if (mid - 1 >= start && a[mid - 1] == k) return true;
+                return search(a, mid + 1, end, k);
+            } else if (a[mid] > k) {
+                if (mid + 1 <= end && a[mid + 1] == k) return true;
+                return search(a, start, mid - 1, k);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 10, 3, 40, 20, 50, 80, 70};
+        log.debug("{}", new P14().solve(a, 10));
+        log.debug("{}", new P14().solve(a, 20));
+        log.debug("{}", new P14().solve(a, 3));
+        log.debug("{}", new P14().solve(a, 40));
+        log.debug("{}", new P14().solve(a, 50));
+        log.debug("{}", new P14().solve(a, 940));
+        log.debug("{}", new P14().solve(a, 90));
+        log.debug("{}", new P14().solve(a, 00));
+        log.debug("{}", new P14().solve(a, 9090));
+    }
+}
---
+++ src/main/java/array/P15.java
@@ -0,0 +1,49 @@
+package array;
+
+/**
+ * K’th Smallest/Largest Element in Unsorted Array. Or quick select.
+ * 
+ * @author harish.sharma
+ *
+ */
+public class P15 {
+    public int solve(int[] a, int k) {
+        int start = 0;
+        int end = a.length - 1;
+        return quickSelect(a, start, end, k);
+    }
+
+    private int quickSelect(int[] a, int start, int end, int k) {
+        int partition = partition(a, start, end, k);
+        if (partition == k)
+            return a[k];
+        else if (partition < k) {
+            return quickSelect(a, partition + 1, end, k - partition);
+        } else {
+            return quickSelect(a, start, partition - 1, k);
+        }
+    }
+
+    private int partition(int[] a, int start, int end, int k) {
+        int pivot = a[end];
+        int i = start, j = end;
+        for (; i < j;) {
+            while (a[i] <= pivot) {
+                if (i >= j) break;
+                i++;
+            }
+            while (a[j] > pivot) {
+                if (i >= j) break;
+                j--;
+            }
+            if (i < j) Util.swap(a, i, j);
+        }
+        if (a[i] > a[end]) Util.swap(a, i, end);
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 5, 4, 2, 9, 3, 11};
+        System.out.println(new P15().solve(a, 3));
+    }
+}
